82fb67f5095267e41fcc50e73dbcb4fc060ad943
alex <camward@yandex.ru>
2022-07-04 22:46:49 +0400

    постраничная навигация

--- src/projects/ProjectOne/components/Passengers/index.tsx
+++ src/projects/ProjectOne/components/Passengers/index.tsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 
-const Passengers = () => {
-  const { isLoading, error, data, isSuccess } = useQuery("passengers", () =>
-    fetch("https://api.instantwebtools.net/v1/passenger?page=0&size=10").then(
-      (res) => res.json()
-    )
+function Passengers() {
+  const [page, setPage] = useState(0);
+
+  const fetchPassengers = async (page: number) => {
+    const res = await fetch(
+      `https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`
+    );
+    return res.json();
+  };
+
+  const { isLoading, error, data, isSuccess } = useQuery(
+    ["passengers", page],
+    () => fetchPassengers(page),
+    { keepPreviousData: false }
   );
 
   return (
-    <ul>
-      {isSuccess &&
-        data.data.map((item: any) => (
-          <React.Fragment key={item._id}>
-            <li>
-              {item._id} - {item.name}
-            </li>
-          </React.Fragment>
-        ))}
-
-      {isLoading && <p>Загрузка...</p>}
-      {error && <p>Ошибка</p>}
-    </ul>
+    <div>
+      <div>
+        <button onClick={() => setPage((old) => Math.max(0, old - 1))}>
+          Назад
+        </button>
+        &nbsp; {page + 1} &nbsp;
+        <button onClick={() => setPage((old) => old + 1)}>Вперед</button>
+        <br />
+        <br />
+      </div>
+      <ul>
+        {isSuccess &&
+          data.data.map((item: any) => (
+            <React.Fragment key={item._id}>
+              <li>
+                {item._id} - {item.name}
+              </li>
+            </React.Fragment>
+          ))}
+        {isLoading && <p>Загрузка...</p>}
+        {error && <p>Ошибка</p>}
+      </ul>
+    </div>
   );
-};
+}
 
 export default Passengers;
