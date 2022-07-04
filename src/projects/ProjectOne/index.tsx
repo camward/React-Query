7fb3d3e03663112cafb7cf541a271cb09a1d903d
alex <camward@yandex.ru>
2022-07-04 22:15:44 +0400

    глобальные настройки React Query

--- src/projects/ProjectOne/index.tsx
+++ src/projects/ProjectOne/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const ProjectOne = () => {
   return (
