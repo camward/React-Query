7e69b1e9082a9149075d94b271aebd5217bd6c7d
alex <camward@yandex.ru>
2022-07-04 22:14:38 +0400

    подключение React Query npm i react-query

--- src/projects/ProjectOne/index.tsx
+++ src/projects/ProjectOne/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const ProjectOne = () => {
   return (
     <div>
       <h3>Project One</h3>
+      <QueryClientProvider client={queryClient}>
+        <div>Content...</div>
+      </QueryClientProvider>
     </div>
   );
 };
