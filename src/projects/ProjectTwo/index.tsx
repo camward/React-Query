10e2673288a03c0257532e547d9ecfea493983f2
alex <camward@yandex.ru>
2022-07-05 21:20:48 +0400

    project 2. подключение React Query. глобальные настройки. Devtools npm i react-query

--- src/projects/ProjectTwo/index.tsx
+++ src/projects/ProjectTwo/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      keepPreviousData: false,
+      retry: 1,
+      retryDelay: 500,
+    },
+  },
+});
 
 const ProjectTwo = () => {
   return (
     <div>
       <h3>Project Two</h3>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 };
